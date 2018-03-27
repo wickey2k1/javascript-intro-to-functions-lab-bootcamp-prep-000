9b65cb26ec8e6f7d5ebae6d25854ff24b41c4368
wickert.michael@gmail.com <wickert.michael@gmail.com>
2018-03-27 17:31:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function logWhisper(string) {
   console.log(lowercase);
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase === string) {
+  if (string.toLowerCase() === string) {
     return ("I can\'t hear you!");
   } 
-  if (string.toUpperCase === string) {
+  if (string.toUpperCase() === string) {
     return ("YES INDEED!");
   } 
   if (string === 'I love you, Grandma.') {
